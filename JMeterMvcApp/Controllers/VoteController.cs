cf63876a93f34e9fa751c5567c6c93d94aa350a6
normalian <warito_hutsu@hotmail.com>
2012-12-16 18:36:44 +0900

    modify

--- JMeterMvcApp/Controllers/VoteController.cs
+++ JMeterMvcApp/Controllers/VoteController.cs
@@ -20,6 +20,7 @@ namespace JMeterApp.Mvc.Controllers
             return View(VoteCandidateFactory.VoteCandidates);
         }
 
+        [HttpGet]
         public ActionResult Select(int id)
         {
             if (id < 0 || id >= VoteCandidateFactory.VoteCandidates.Count())
@@ -27,6 +28,29 @@ namespace JMeterApp.Mvc.Controllers
                 TempData["Message"] = "選択が不正です('A`)。再度選択してください。";
                 return RedirectToAction("Index");
             }
+
+            return View(VoteCandidateFactory.VoteCandidates[id]);
+        }
+
+        [HttpPost]
+        public ActionResult Select(FormCollection formCollection)
+        {
+            string comment = formCollection["comment"];
+            int id = 0;
+            if (int.TryParse(formCollection["id"], out id) == false)
+            {
+                TempData["Message"] = "選択が不正っぽいです('A`)。再度投票してください。";
+                return RedirectToAction("Index");
+            }
+            TempData["Comment"] = comment;
+            TempData["Id"] = id;
+            return RedirectToAction("End");
+        }
+
+        [HttpPost]
+        public ActionResult End()
+        {
+            ViewBag.Comment = TempData["Comment"];
 
             return View();
         }
